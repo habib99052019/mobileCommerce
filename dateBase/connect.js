a51b30e64ec7c525db22fd720347ffa7b03f750a
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2024-01-16 10:18:00 +0100

    Update connect.js

--- dateBase/connect.js
+++ dateBase/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //mongoose.connect('mongodb+srv://webmaster:webmaster123@cluster0.jc0r9.mongodb.net/backKoorea',
-mongoose.connect('mongodb+srv://webmaster:webmaster123@cluster0.jc0r9.mongodb.net/backKooreaAZ',
+mongoose.connect('mongodb+srv://webmaster:webmaster123@cluster0.jc0r9.mongodb.net/backKooreaAZk',
 {useNewUrlParser: true, 
 useUnifiedTopology: true,
 }).then(()=>console.log('Successfully connected to database.')).catch((e)=>console.error('Error in connection',e));
